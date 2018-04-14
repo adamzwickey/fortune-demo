b6da452a4e2ff63ecfb1a1e5d112900f0ee86d3f
azwickey-pivotal <vania12R>
2018-04-14 11:37:48 -0400

    paramaterized app name

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -16,4 +16,4 @@ cd git-assets/
 mvn -DskipTests=true package
 
 echo "saving file with version number: $VERSION"
-cp target/*.jar ../$CF_APP-releases/$CF_APP-$VERSION.jar
+cp target/*.jar ../releases/$CF_APP-$VERSION.jar
